28f79d4d471af3c7f11bd04f1485d71de08ee794
PajasShinji <luispower432@hotmail.com>
2025-02-02 12:03:18 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+const messages = [
+    "¿Estas segura?",
+    "¿Realmente segura?",
+    "¿Deberas deberitas?",
+    "Kiri por favor...",
+    "¡Sólo piénsalo!",
+    "Si dices que no, estaré muy triste....",
+    "Estaré muy triste...",
+    "Estaré muy muy muy triste...",
+    "Está bien, dejaré de preguntar....",
+    "¡Es broma, di que sí, por favor! ❤️"
+];
+
+let messageIndex = 0;
+
+function handleNoClick() {
+    const noButton = document.querySelector('.no-button');
+    const yesButton = document.querySelector('.yes-button');
+    noButton.textContent = messages[messageIndex];
+    messageIndex = (messageIndex + 1) % messages.length;
+    const currentSize = parseFloat(window.getComputedStyle(yesButton).fontSize);
+    yesButton.style.fontSize = `${currentSize * 1.5}px`;
+}
+
+function handleYesClick() {
+    window.location.href = "yes.html";
+}
